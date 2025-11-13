edd185341a2fc48e6171453f7bf21939be421ff9
m1ga <migamiga@web.de>
2025-11-13 16:08:53 +0100

    use old lib locally

--- android/src/ti/blurview/BlurViewProxy.java
+++ android/src/ti/blurview/BlurViewProxy.java
@@ -11,7 +11,6 @@ import android.app.Activity;
 import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.FrameLayout;
 
 import com.github.mmin18.widget.RealtimeBlurView;
 
